c9d3aa0b9688bbd646c4028c71a5845b9ece5785
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-15 16:16:09 +0100

    Added expanding about sections

---
+++ script.js
@@ -0,0 +1,29 @@
+const expanderTitle = document.querySelectorAll(".expander__list-item--clickable");
+const expandingSection = document.querySelectorAll(".expander__list-item--expand");
+
+//Do not display by default
+expandingSection.forEach((section) => {
+    section.style.display = "none";
+})
+
+const expandItem = (section) => {
+    //If not diplayed, display, and change + to -
+    if (section.nextElementSibling.style.display == "none"){
+        section.nextElementSibling.style.display = "inline-block";
+        section.lastChild.previousSibling.src = "./assets/icons/minus-icon.svg";
+
+    //If displayed, hide, and change - to +
+    } else if(section.nextElementSibling.style.display == "inline-block"){
+        section.nextElementSibling.style.display = "none";
+        section.lastChild.previousSibling.src = "./assets/icons/plus-icon.svg";
+        
+    } else{
+        return;
+    }
+}
+
+expanderTitle.forEach((section) => {
+    section.addEventListener("click", () => {
+        expandItem(section);
+    });
+})
